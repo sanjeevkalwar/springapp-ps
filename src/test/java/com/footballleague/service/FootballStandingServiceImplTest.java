71d6e409b014669e15d5d6ecd76b925dadb38748
Sanjeev Kalwar <sanjeevkalwar@gmail.com>
2021-02-08 23:25:50 +0530

    FootballStandingServiceImplTest formatted

--- src/test/java/com/footballleague/service/FootballStandingServiceImplTest.java
+++ src/test/java/com/footballleague/service/FootballStandingServiceImplTest.java
@@ -1,5 +1,9 @@
 package com.footballleague.service;
 
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -9,26 +13,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.*;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.footballleague.exception.TeamNotFoundException;
 import com.footballleague.model.League;
 import com.footballleague.model.Standing;
 import com.footballleague.model.StandingInfo;
-import com.footballleague.service.FootballStandingApiServiceImpl;
-import com.footballleague.service.FootballStandingService;
-import com.footballleague.service.LeagueService;
 import com.footballleague.utils.DataPopulation;
+
 @RunWith(MockitoJUnitRunner.class)
 public class FootballStandingServiceImplTest {
-	
+
 	@InjectMocks
 	private FootballStandingService footballStandingService;
-	
+
 	@Mock
 	private FootballStandingApiServiceImpl footballStandingApiServiceImpl;
 
@@ -37,29 +35,27 @@ public class FootballStandingServiceImplTest {
 
 	@Mock
 	private LeagueService leagueService;
-	Map<String,String> countryMap=Map.of("England", "41");
-	List<League> leagueList=new ArrayList<League>();
-	List<Standing> standingInfo=new ArrayList<Standing>();
-	
+	Map<String, String> countryMap = Map.of("England", "41");
+	List<League> leagueList = new ArrayList<League>();
+	List<Standing> standingInfo = new ArrayList<Standing>();
+
 	@Before
 	public void setUp() {
-		League league=new League();
+		League league = new League();
 		league.setCountryId("41");
 		league.setCountryName("England");
 		league.setLeagueId("149");
 		league.setLeagueName("Championship");
 		leagueList.add(league);
 		when(dataPopulation.getCountries()).thenReturn(countryMap);
-		when(leagueService
-				.getLeaguesForACountryFromApi(anyString())).thenReturn(leagueList);
-		when(footballStandingApiServiceImpl.getStandingsInfoByLeagueId(anyString()))
-		.thenReturn(standingInfo);
+		when(leagueService.getLeaguesForACountryFromApi(anyString())).thenReturn(leagueList);
+		when(footballStandingApiServiceImpl.getStandingsInfoByLeagueId(anyString())).thenReturn(standingInfo);
 	}
-	
+
 	@Test(expected = TeamNotFoundException.class)
 	public void getDataForStandings() {
-		List<StandingInfo> info=footballStandingService
-				.getStandingInfoForATeamInALeague("England","Championship","Birmingham",false);
+		List<StandingInfo> info = footballStandingService.getStandingInfoForATeamInALeague("England", "Championship",
+				"Birmingham", false);
 		assertNotNull(info);
 	}
 
